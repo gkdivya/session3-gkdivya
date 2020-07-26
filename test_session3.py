065071045cb58f7ff48525fd0ec6bc1496c1cc89
gkdivya <gkdivya@gmail.com>
2020-07-27 02:19:43 +0530

    Encoding UTF-8 added

--- test_session3.py
+++ test_session3.py
@@ -44,14 +44,14 @@ def test_readme_exists():
     assert os.path.isfile("README.md"), "README.md file missing!"
 
 def test_readme_contents():
-    readme = open("README.md", "r")
+    readme = open("README.md", "r", encoding="utf-8")
     readme_words = readme.read().split()
     readme.close()
     assert len(readme_words) >= 500, "Make your README.md file interesting! Add atleast 500 words"
 
 def test_readme_proper_description():
     READMELOOKSGOOD = True
-    f = open("README.md", "r")
+    f = open("README.md", "r", encoding="utf-8")
     content = f.read()
     f.close()
     for c in README_CONTENT_CHECK_FOR:
